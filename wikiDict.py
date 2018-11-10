9397d985e47372b4704ed2ecc5b83ec1db0d9409
annapark1004 <44926336+annapark1004@users.noreply.github.com>
2018-11-10 15:08:27 -0500

    Update wikiDict.py

--- wikiDict.py
+++ wikiDict.py
@@ -9,35 +9,51 @@ import wikipedia
 import networkx as nx
 import json
 
-def formatDataset(filename):
-    wikiSearch=dict()
+##With a file:
+# def formatDataset(filename):
+#     wikiSearch=dict()
+# 
+#     with open(filename, 'rb') as json_file:  
+#         mixedDict = json.load(json_file)
+# 
+#     for mixedInterests in mixedDict:
+#         mixedInterests.lower()
+#         try:
+#             searchWiki = wikipedia.summary(mixedInterests)
+#             wikiSearch[mixedInterests]= searchWiki
+#                 
+#         except wikipedia.exceptions.DisambiguationError as e:
+#             failSearch = wikipedia.search(mixedInterests)
+#             
+#             print("Search may refer to: ")
+#             for i, failSearch in enumerate(failSearch):
+#                 print(i, failSearch)
+#             choice=int(input("Enter a choice: "))
+#             assert choice in range(len(failSearch))
+#             failSearchWiki = wikipedia.summary(failSearch)
+#             wikiSearch[failSearch] = failSearchWiki
+#     
+#     return wikiSearch
+    
+    
+    
+##With a key word:
 
-    with open(filename, 'rb') as json_file:  
-        mixedDict = json.load(json_file)
-
-    for mixedInterests in mixedDict:
-        mixedInterests.lower()
-        try:
-            searchWiki = wikipedia.summary(mixedInterests)
-            wikiSearch[mixedInterests]= searchWiki
-                
-        except wikipedia.exceptions.DisambiguationError as e:
-            failSearch = wikipedia.search(mixedInterests)
+def wikiDict(keyWord):
+    try:
+        searchWiki = wikipedia.summary(keyWord)
+        return searchWiki
             
-            print("Search may refer to: ")
-            for i, failSearch in enumerate(failSearch):
-                print(i, failSearch)
-            choice=int(input("Enter a choice: "))
-            assert choice in range(len(failSearch))
-            failSearchWiki = wikipedia.summary(failSearch)
-            wikiSearch[failSearch] = failSearchWiki
-    
-    return wikiSearch
-    
-
-
-#formatDataset('Physics.txt')
-
+    except wikipedia.exceptions.DisambiguationError as e:
+        failSearch = wikipedia.search(mixedInterests)
+        
+        print("Search may refer to: ")
+        for i, failSearch in enumerate(failSearch):
+            print(i, failSearch)
+        choice=int(input("Enter a choice: "))
+        assert choice in range(len(failSearch))
+        failSearchWiki = wikipedia.summary(failSearch)
+        return failSearchWiki
 
 
 
